1c35f7f04f187d07310f5fdd82a7169f20951c13
Alexis Cano <aic918_aicano918@gmail.com>
2025-12-15 13:25:39 -0600

    google meets

--- mentora/urls.py
+++ mentora/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.urls import path
+from . import views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path("accounts/", include("django.contrib.auth.urls")),  # login/logout/password views
+    path("schedule/", views.schedule, name="schedule"),
 ]
 
 if settings.DEBUG:
