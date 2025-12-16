bec2f2d99c80bb050243be76c175cb3b9635c397
Alexis Cano <aic918_aicano918@gmail.com>
2025-12-15 20:17:41 -0600

    Fixed migration bug

--- core/migrations/0009_notification.py
+++ core/migrations/0009_notification.py
@@ -8,8 +8,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0008_follow"),
-    ]
+    ("core", "0001_initial"),
+]
+
 
     operations = [
         migrations.CreateModel(
--- core/migrations/0009_profile_calendly_url.py
+++ core/migrations/0009_profile_calendly_url.py
@@ -6,8 +6,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0008_follow"),
-    ]
+    ("core", "0001_initial"),
+]
+
 
     operations = [
         migrations.AddField(
